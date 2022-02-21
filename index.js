c01de79fe692187a76a214694f7d98ffd1e1168a
Ibrahima <ibracool99@gmail.com>
2022-02-21 23:18:11 +0100

    exec

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 import xlsx from 'node-xlsx';
 import * as fs from "fs"
+
+const exec = require('@actions/exec');
+
+await exec.exec('npm i');
 try {
     const zapRawData = fs.readFileSync(core.getInput("zap"));
     const zap = JSON.parse(zapRawData);
