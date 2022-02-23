078b3654760142eef84227b8b23fccbdcf4e1dc8
Ibrahima <ibracool99@gmail.com>
2022-02-23 10:05:50 +0100

    mobsf try fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ try {
         })
     }
     const buffer = xlsx.build(sheets);
-    const path = "report.xlsx";
+    const path = "/home/runner/work/_temp/_github_home/report.xlsx";
     fs.createWriteStream(path).write(buffer);
 } catch (error) {
     core.setFailed(error.message);
